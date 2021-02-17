6c4c7f64170c2f69921dd7d9b187c4d858238771
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2021-02-16 18:37:41 -0800

    Sistema experto ya funcional

--- SistemaExpertoPokeDex.lisp
+++ SistemaExpertoPokeDex.lisp
@@ -7,28 +7,17 @@
 
 ;;; Clase Pokedex **************************************************
 
+;;; Para definir los tipos en el cual son efectivos
 (setf tiposE (make-array '(6)))
 
-(setf (aref tiposE 0) "Fuego y ")
-(setf (aref tiposE 1) "Agua y ")
-(setf (aref tiposE 2) "Planta y")
-(setf (aref tiposE 3) "Hielo y ")
-(setf (aref tiposE 4) "Plata y Bicho")
-(setf (aref tiposE 5) "Agua y Planta")
-
-(defun get-pokemon (tipo)
-    (case tipo
-        (1 (print "Agua")
-           (print "Y más Agua"))
-        (2 (print "Planta"))
-        (3 (print "Fuego"))
-        (4 (print "Acero"))
-        (5 (print "Volador"))
-        (6 (print "Hielo"))
-        (otherwise (print "No valido"))))
+(setf (aref tiposE 0) "Fuego, Roca y Tierra")
+(setf (aref tiposE 1) "Agua, Roca y Tierra ")
+(setf (aref tiposE 2) "Planta, Acero, Hielo y Bicho")
+(setf (aref tiposE 3) "Hielo, Roca y Hada")
+(setf (aref tiposE 4) "Plata, Bicho y Lucha")
+(setf (aref tiposE 5) "Volador, Planta y Dragon")
 
 ;;; Clase Vista ****************************************************
-
 (defun bienvenida ()
     (print "Bienvenido a la tabla de tipos de pokemon"))
 
@@ -40,11 +29,30 @@
     (print "4. Acero")
     (print "5. Volador")
     (print "6. Hielo")
-    (print "-> "))
+    (print "-> "))    
+
+(defun get-pokemon (tipo)
+    (case tipo
+        (1 (print "Agua"))
+        (2 (print "Planta"))
+        (3 (print "Fuego"))
+        (4 (print "Acero"))
+        (5 (print "Volador"))
+        (6 (print "Hielo"))
+        (otherwise (print "No valido"))))
     
+(defun aviso (numero)
+    (case numero
+        (1 (print "Te pasaste con el maximo"))
+        (2 (print "Te pasaste con el minimo"))))
+        
+(defun efectivo ()
+    (print "Es efectivo contra: "))
+    
+(defun invalido ()
+    (print "No ingreso un tipo de pokemon válido"))
 
 ;;; Clase Controlador & Main ***************************************
-
 (bienvenida)
 
 (menut1) ;;; Para mostrar los tipos de pokemon
@@ -55,3 +63,22 @@
 
 ;;; Para asignar los tipos correspondientes
 (get-pokemon b)
+
+;;; Demostrar donde es efectivo
+
+(defvar *control* 0)
+
+(if (>= b 7)
+    (aviso 1)
+    (setf *control* (+ *control* 1)))
+    
+(if (<= b 0)
+    (aviso 2)
+    (setf *control* (+ *control* 1)))
+
+(efectivo)
+
+;;; Vamos a verificar si todo esta en orden
+(if (= *control* 2)
+    (print (aref tiposE (- b 1)))
+    (invalido))
